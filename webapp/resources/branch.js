1e8dbfc1953ce17e5ba6f128b992c53146b5eabd
Casper27-th <Casper227799@gmail.com>
2020-11-08 00:26:01 +0800

    cteates

--- webapp/resources/branch.js
+++ webapp/resources/branch.js
@@ -1,5 +1,26 @@
-
-function mouseOver(){
-    console.log(mycolor.item);  
-};
-
+window.onload = function(){
+    let createtheme = document.getElementById("crTheme");
+    let modalcrtheme = document.getElementById("createThemeField");
+    let createtask = document.getElementById("crTask");
+    let modalcrtask = document.getElementById("createTaskField");
+    
+     createtheme.onclick = function(){
+        modalcrtheme.style.display="block";
+        modalcrtheme.onclick = function(event) {
+            if(!event.target.className.includes("createForm")){
+            modalcrtheme.style.display = "none";  }      
+        }
+    };  
+    createtask.onclick = function(){
+        modalcrtask.style.display="block";
+        modalcrtask.onclick = function(event) {
+            if(!event.target.className.includes("createForm")){
+            modalcrtask.style.display = "none";  }      
+        }
+    };  
+    let del = document.getElementById("deltask");
+    del.onclick = function(event) {
+        event.stopPropagation();
+    }
+    
+}

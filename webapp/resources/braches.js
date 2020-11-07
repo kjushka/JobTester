02314b4bdfa8e1683592b22d7063dc30a1a6f3af
Casper27-th <Casper227799@gmail.com>
2020-11-07 17:29:56 +0800

    pluss

--- webapp/resources/braches.js
+++ webapp/resources/braches.js
@@ -1,25 +1,9 @@
 
 window.onload = function(){
-    let login = document.getElementById("loginopen");
-    let modallog = document.getElementById("loginField");
+   
     let create = document.getElementById("createBranch");
-    let modalcr = document.getElementById("createField");
-    let reg = document.getElementById("regopen");
-    let modalreg = document.getElementById("registrField");
-    login.onclick = function(){
-        modallog.style.display="block";
-        modallog.onclick = function(event) {
-            if(!event.target.className.includes("loginForm")){
-            modallog.style.display = "none";  }      
-        }
-    };
-    reg.onclick = function(){
-        modalreg.style.display="block";
-        modalreg.onclick = function(event) {
-            if(!event.target.className.includes("regForm")){
-            modalreg.style.display = "none";  }      
-        }
-    };   
+    let modalcr = document.getElementById("loginField");
+
      create.onclick = function(){
         modalcr.style.display="block";
         modalcr.onclick = function(event) {
---
+++ webapp/resources/home.js
@@ -0,0 +1,22 @@
+
+window.onload = function(){
+    let login = document.getElementById("loginopen");
+    let modallog = document.getElementById("loginField");
+    let reg = document.getElementById("regopen");
+    let modalreg = document.getElementById("registrField");
+    login.onclick = function(){
+        modallog.style.display="block";
+        modallog.onclick = function(event) {
+            if(!event.target.className.includes("loginForm")){
+            modallog.style.display = "none";  }      
+        }
+    };
+    reg.onclick = function(){
+        modalreg.style.display="block";
+        modalreg.onclick = function(event) {
+            if(!event.target.className.includes("regForm")){
+            modalreg.style.display = "none";  }      
+        }
+    };   
+    
+}
